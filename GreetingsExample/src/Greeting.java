9db490e8e63149f312cde6961d7d5cc7efbcc5af
Slavena Koleva <slavena_kolevaa@abv.bg>
2017-02-25 16:39:42 +0200

    Added method greetEnglish()

---
+++ GreetingsExample/src/Greeting.java
@@ -0,0 +1,14 @@
+
+public class Greeting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeting gr = new Greeting();
+		gr.greetEnglish();
+	}
+	
+	public void greetEnglish(){
+		System.out.println("Hello");
+	}
+
+}
